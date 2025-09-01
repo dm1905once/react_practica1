4937238b8cb6d125f5190bcfc5d5c73c5dc8c134
danielm <dm1905once@gmail.com>
2025-08-31 20:39:53 -0700

    basic complete layout

--- src/WorldTyper.jsx
+++ src/WorldTyper.jsx
@@ -6,9 +6,9 @@ function WorldTyper() {
         <>
             <div className="text-2xl m-6">
                 <h1 className="text-center">World Typer app</h1>
-                <div className=" w-9/12 border-green-900 border-4 justify-self-center">
-                    <div className="grid grid-cols-3 gap-2 p-3 m-2 border-black border-4">
-                        <div className="flex flex-col items-center border-1">
+                <div className=" w-9/12 border-amber-600 border-2 rounded-2xl justify-self-center">
+                    <div className="grid grid-cols-3 gap-2 p-3 m-2">
+                        <div className="flex flex-col items-center ">
                             <div className="flex-row m-3 px-1 space-x-3">
                                 <button className="buttonLetter">á</button>
                                 <button className="buttonLetter">ā</button>
@@ -18,7 +18,15 @@ function WorldTyper() {
                             </div>
                         </div>
 
-                        <div className="h-80 col-span-2 bg-blue-500 "></div>
+                        <div className="h-80 col-span-2">
+                            <div className="flex flex-col items-center mt-3  ">
+                                <textarea className="textArea" defaultValue={"af"}></textarea>
+                                <div>
+                                    <button className="buttonAction">Copy</button>
+                                    <button className="buttonAction">Clear</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
