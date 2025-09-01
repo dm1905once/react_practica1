7dccee3faa92a8a46993fb2df5e26aef7bca4c1e
danielm <dm1905once@gmail.com>
2025-08-31 21:40:52 -0700

    first goal - does the job

--- src/WorldTyper.jsx
+++ src/WorldTyper.jsx
@@ -1,6 +1,25 @@
 import './WorldTyper.css';
+import {useState} from "react";
 
 function WorldTyper() {
+    const [inputText, setInputText] = useState("");
+
+    function typeLetter(event) {
+        setInputText(prevState => prevState + event.target.textContent);
+    }
+
+    function refreshTextArea(event){
+        setInputText(event.target.value);
+    }
+
+    function clearTextArea(){
+        setInputText("");
+    }
+
+    const copyToClipboard = async (event) => {
+        await navigator.clipboard.writeText(inputText);
+        clearTextArea();;
+    }
 
     return (
         <>
@@ -10,20 +29,38 @@ function WorldTyper() {
                     <div className="grid grid-cols-3 gap-2 p-3 m-2">
                         <div className="flex flex-col items-center ">
                             <div className="flex-row m-3 px-1 space-x-3">
-                                <button className="buttonLetter">á</button>
-                                <button className="buttonLetter">ā</button>
+                                <button className="buttonLetter" onClick={typeLetter}>á</button>
+                                <button className="buttonLetter" onClick={typeLetter}>ā</button>
                             </div>
                             <div className="flex-row m-3 px-1 space-x-3">
-                                <button className="buttonLetter">ē</button>
+                                <button className="buttonLetter" onClick={typeLetter}>ē</button>
+                            </div>
+                            <div className="flex-row m-3 px-1 space-x-3">
+                                <button className="buttonLetter" onClick={typeLetter}>ī</button>
+                            </div>
+                            <div className="flex-row m-3 px-1 space-x-3">
+                                <button className="buttonLetter" onClick={typeLetter}>š</button>
+                            </div>
+                            <div className="flex-row m-3 px-1 space-x-3">
+                                <button className="buttonLetter" onClick={typeLetter}>ú</button>
+                            </div>
+                            <div className="flex-row m-3 px-1 space-x-3">
+                                <button className="buttonLetter" onClick={typeLetter}>ļ</button>
+                            </div>
+                            <div className="flex-row m-3 px-1 space-x-3">
+                                <button className="buttonLetter" onClick={typeLetter}>ņ</button>
+                            </div>
+                            <div className="flex-row m-3 px-1 space-x-3">
+                                <button className="buttonLetter" onClick={typeLetter}>ņ</button>
                             </div>
                         </div>
 
                         <div className="h-80 col-span-2">
                             <div className="flex flex-col items-center mt-3  ">
-                                <textarea className="textArea" defaultValue={"af"}></textarea>
+                                <textarea rows="4" cols="30" className="textArea" value={inputText} onChange={refreshTextArea}></textarea>
                                 <div>
-                                    <button className="buttonAction">Copy</button>
-                                    <button className="buttonAction">Clear</button>
+                                    <button className="buttonAction" onClick={copyToClipboard}>Copy</button>
+                                    <button className="buttonAction" onClick={clearTextArea}>Clear</button>
                                 </div>
                             </div>
                         </div>
